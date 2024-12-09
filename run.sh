009786fa9879aabcfa140d0b9ff9c95d73cc98a6
MagisterLatka <michallatka3@wp.pl>
2024-12-09 09:56:28 +0100

    Changed run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
-g++ main.cpp -o komiwojazer -std=c++23
-./komiwojazer
+g++ tsp_aco_m.cpp -o bin/tsp_m -std=c++23 -O3
+echo 'Berlin52'
+./bin/tsp_m < tests/berlin52.in
+echo 'Bier127'
+./bin/tsp_m < tests/bier127.in
+echo 'Tsp250'
+./bin/tsp_m < tests/tsp250.in
+echo 'Tsp500'
+./bin/tsp_m < tests/tsp500.in
+echo 'Tsp1000'
+./bin/tsp_m < tests/tsp1000.in
